ddae5c03099561b03a2e8bad121c339c96d4698d
Junwoo Hwang <zzulu.hwang@gmail.com>
2020-12-22 14:39:57 +0900

    Support for thread

--- app.py
+++ app.py
@@ -12,47 +12,52 @@ app = App(
 
 
 @app.message(re.compile(r'(<https://www\.notion\.so/.+>)'))
-def message_notion_url(client, message):
+def message_notion_url(client, message, body):
     channel_id = message['channel']
     target_message_ts = message['ts']
-    res = client.chat_postEphemeral(
-        channel=channel_id,
-        user=message['user'],
-        blocks=[
+    target_message_thread_ts = message.get('thread_ts', '')
+    options = {
+        'channel': channel_id,
+        'user': message['user'],
+        'blocks': [
             {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "Notion Web URL(*https://*)을 사용하셨네요! Notion App URL(*notion://*)도 필요하신가요?"
-                }
+                'type': 'section',
+                'text': {
+                    'type': 'mrkdwn',
+                    'text': 'Notion Web URL(*https://*)을 사용하셨네요! Notion App URL(*notion://*)도 필요하신가요?',
+                },
             },
             {
-                "type": "actions",
-                "block_id": "notion_fairy_dialog",
-                "elements": [
+                'type': 'actions',
+                'block_id': 'notion_fairy_dialog',
+                'elements': [
                     {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "네!"
+                        'type': 'button',
+                        'text': {
+                            'type': 'plain_text',
+                            'text': '네!'
                         },
-                        "style": "primary",
-                        "action_id": "notion_fairy_true",
-                        "value": f"{target_message_ts}"
+                        'style': 'primary',
+                        'action_id': 'notion_fairy_true',
+                        'value': f'{target_message_ts},{target_message_thread_ts}',
                     },
                     {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "아니요.."
+                        'type': 'button',
+                        'text': {
+                            'type': 'plain_text',
+                            'text': '아니요..',
                         },
-                        "style": "danger",
-                        "action_id": "notion_fairy_false",
-                    }
+                        'style': 'danger',
+                        'action_id': 'notion_fairy_false',
+                    },
                 ]
-            }
-        ]
-    )
+            },
+        ],
+    }
+    if target_message_thread_ts:
+        options['thread_ts'] = target_message_thread_ts
+
+    res =  client.chat_postEphemeral(**options)
 
 
 @app.action(re.compile('notion_fairy_(true|false)'))
@@ -68,15 +73,24 @@ def notion_fairy_button(client, ack, say, body, payload):
 
     if payload['action_id'] == 'notion_fairy_true':
         container_channel_id = body['container']['channel_id']
-        target_message_ts = payload['value']
+        target_message_ts, target_message_thread_ts = payload['value'].split(',')
 
-        # Fetch Message with ts
-        slack_response = client.conversations_history(
-            channel=container_channel_id,
-            latest=target_message_ts,
-            limit=1,
-            inclusive=True,
-        )
+        # Fetch Message with ts (and thread ts)
+        if target_message_thread_ts:
+            slack_response = client.conversations_replies(
+                channel=container_channel_id,
+                ts=target_message_ts,
+                latest=target_message_thread_ts,
+                limit=1,
+                inclusive=True,
+            )
+        else:
+            slack_response = client.conversations_history(
+                channel=container_channel_id,
+                latest=target_message_ts,
+                limit=1,
+                inclusive=True,
+            )
         text = slack_response['messages'][0]['text']
 
         # Replace https to notion
@@ -84,7 +98,14 @@ def notion_fairy_button(client, ack, say, body, payload):
         matches_with_newline = '\n'.join(pattern.findall(text))
         edited_text = matches_with_newline.replace('https', 'notion')
 
-        say(edited_text)
+        # Post Message
+        options = {
+            'channel': container_channel_id,
+            'text': edited_text,
+        }
+        if target_message_thread_ts:
+            options['thread_ts'] = target_message_thread_ts
+        slack_response = client.chat_postMessage(**options)
 
 
 @app.event('message')
