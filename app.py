5c1009ef5fccf82585a8c623bb1c0698c07be9a1
Junwoo Hwang <zzulu.hwang@gmail.com>
2021-01-06 01:06:29 +0900

    Bot message will be updated when user update their messages

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 import logging
 from slack_bolt import App
 from slack_bolt.adapter.aws_lambda import SlackRequestHandler
+import boto3
 
 
 app = App(
@@ -13,15 +14,10 @@ app = App(
 )
 
 
-def create_fairy_dialog(message):
-    channel_id = message['channel']
-    if message.get('subtype', '') == 'message_changed':
-        message = message['message']
-    target_message_ts = message['ts']
-    target_message_thread_ts = message.get('thread_ts', '')
+def create_fairy_dialog(channel: str, target_message_ts: str, target_message_thread_ts: str, user: str) -> dict:
     options = {
-        'channel': channel_id,
-        'user': message['user'],
+        'channel': channel,
+        'user': user,
         'blocks': [
             {
                 'type': 'section',
@@ -64,16 +60,45 @@ def create_fairy_dialog(message):
 
 @app.message(re.compile(r'(<https://www\.notion\.so/.+>)'))
 def catch_notion_web_url(client, message):
-    options = create_fairy_dialog(message)
+    channel = message['channel']
+    target_message_ts = message['ts']
+    target_message_thread_ts = message.get('thread_ts', '')
+    user = message['user']
+
+    options = create_fairy_dialog(channel, target_message_ts, target_message_thread_ts, user)
     res = client.chat_postEphemeral(**options)
 
 
 @app.event({'type': 'message', 'subtype': 'message_changed'})
 def catch_edited_notion_web_url(client, message):
     pattern = re.compile(r'(<https://www\.notion\.so/.+>)')
-    if pattern.findall(message['message']['text']) != pattern.findall(message['previous_message']['text']):
-        options = create_fairy_dialog(message)
-        res = client.chat_postEphemeral(**options)
+    matches = pattern.findall(message['message']['text'])
+    if matches != pattern.findall(message['previous_message']['text']):
+        channel = message['channel']
+        target_message_ts = message['message']['ts']
+        target_message_thread_ts = message['message'].get('thread_ts', '')
+        user = message['message']['user']
+
+        fairy_ts = fetch_fairy_ts(target_message_ts)
+        if fairy_ts:
+            edited_text = '\n'.join(matches).replace('https', 'notion')
+            client.chat_update(channel=channel, ts=fairy_ts, text=edited_text)
+        else:
+            options = create_fairy_dialog(channel, target_message_ts, target_message_thread_ts, user)
+            res = client.chat_postEphemeral(**options)
+
+
+def fetch_fairy_ts(origin_ts: str) -> str:
+    dynamodb = boto3.client('dynamodb')
+    response = dynamodb.get_item(TableName=config('AWS_DYNAMODB_TABLE_NAME'),
+                                 Key={'OriginTs':{'S':origin_ts}})
+    return response['Item']['FairyTs']['S'] if 'Item' in response else ''
+
+
+def create_connection(origin_ts: str, fairy_ts: str) -> None:
+    dynamodb = boto3.client('dynamodb')
+    response = dynamodb.put_item(TableName=config('AWS_DYNAMODB_TABLE_NAME'),
+                                 Item={'OriginTs':{'S':origin_ts},'FairyTs':{'S':fairy_ts}})
 
 
 @app.action(re.compile('notion_fairy_(true|false)'))
@@ -114,7 +139,7 @@ def notion_fairy_button(client, ack, say, body, payload):
         matches_with_newline = '\n'.join(pattern.findall(text))
         edited_text = matches_with_newline.replace('https', 'notion')
 
-        # Post Message
+        # Post message
         options = {
             'channel': container_channel_id,
             'text': edited_text,
@@ -123,14 +148,18 @@ def notion_fairy_button(client, ack, say, body, payload):
             options['thread_ts'] = target_message_thread_ts
         slack_response = client.chat_postMessage(**options)
 
+        # Create connection between origin and fairy messages
+        create_connection(target_message_ts, slack_response['message']['ts'])
+
 
 @app.event('message')
 def do_nothing():
     pass
 
 
-SlackRequestHandler.clear_all_log_handlers()
-logging.basicConfig(format="%(asctime)s %(message)s", level=logging.DEBUG)
+if config('AWS_LAMBDA', default=False, cast=bool):
+    SlackRequestHandler.clear_all_log_handlers()
+    logging.basicConfig(format="%(asctime)s %(message)s", level=logging.DEBUG)
 
 
 def lambda_handler(event, context):
